ec9e7de5e577027ad9d27c393387b7515ba4fdc6
Alan Hynes r00145398 <alan.hynes@mycit.ie>
2018-11-27 17:49:38 +0000

    created a java file and a basic program

---
+++ javaTravisTest.java
@@ -0,0 +1,16 @@
+public class simpleprogram2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is testing if Travis works!");
+		
+		int number = 5;
+		
+		int multiple = 4;
+		
+		System.out.println(number * multiple);
+
+	}
+
+}
